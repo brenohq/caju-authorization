9699ab12d225340d758e6544554d2f04c31a2eed
brenohq <brenosc2@hotmail.com>
2024-07-08 20:31:41 -0300

    unit tests

--- src/test/java/com/brenohq/caju_authorization/CajuAuthorizationApplicationTests.java
+++ src/test/java/com/brenohq/caju_authorization/CajuAuthorizationApplicationTests.java
@@ -1,13 +1,21 @@
 package com.brenohq.caju_authorization;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.brenohq.caju_authorization.controller.TransactionController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CajuAuthorizationApplicationTests {
 
+	@Autowired
+	private TransactionController transactionController;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(transactionController).isNotNull();
 	}
 
 }
---
+++ src/test/java/com/brenohq/caju_authorization/TransactionControllerTests.java
@@ -0,0 +1,35 @@
+package com.brenohq.caju_authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TransactionControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void nonExistingRouteReturns404() throws Exception {
+        mockMvc.perform(get("/transactions"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[0].accountId").value("01"))
+                .andExpect(jsonPath("$[0].amount").value(100.00))
+                .andExpect(jsonPath("$[0].mcc").value("5432"))
+                .andExpect(jsonPath("$[0].merchant").value("UBER"))
+                .andExpect(jsonPath("$[1].id").value(2))
+                .andExpect(jsonPath("$[1].accountId").value("02"))
+                .andExpect(jsonPath("$[1].amount").value(50.00))
+                .andExpect(jsonPath("$[1].mcc").value("5431"))
+                .andExpect(jsonPath("$[1].merchant").value("IFOOD"));
+    }
+}
