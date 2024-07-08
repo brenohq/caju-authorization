0aeea030281faee34f4ae10359660cb9b45a73ca
brenohq <brenosc2@hotmail.com>
2024-07-08 17:39:17 -0300

    initial setup with spring initializr

---
+++ src/test/java/com/brenohq/caju_authorization/CajuAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brenohq.caju_authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CajuAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
