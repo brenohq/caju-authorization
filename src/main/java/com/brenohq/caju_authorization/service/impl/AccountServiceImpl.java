40e9e358399518e6404c5ded4d6676102175c391
brenohq <brenosc2@hotmail.com>
2024-07-11 14:54:34 -0300

    authorize method logic with data persistence

--- src/main/java/com/brenohq/caju_authorization/service/impl/AccountServiceImpl.java
+++ src/main/java/com/brenohq/caju_authorization/service/impl/AccountServiceImpl.java
@@ -1,22 +1,29 @@
 package com.brenohq.caju_authorization.service.impl;
 
+import com.brenohq.caju_authorization.mapper.BalanceMapper;
 import com.brenohq.caju_authorization.model.Account;
 import com.brenohq.caju_authorization.model.AuthorizationResponse;
 import com.brenohq.caju_authorization.model.Transaction;
 import com.brenohq.caju_authorization.model.dto.CreateAccountDto;
 import com.brenohq.caju_authorization.repository.AccountRepository;
+import com.brenohq.caju_authorization.repository.TransactionRepository;
 import com.brenohq.caju_authorization.service.AccountService;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 @Service
 public class AccountServiceImpl implements AccountService {
     private final AccountRepository accountRepository;
+    private final TransactionRepository transactionRepository;
 
-    public AccountServiceImpl(AccountRepository accountRepository) {
+    public AccountServiceImpl(AccountRepository accountRepository, TransactionRepository transactionRepository) {
         this.accountRepository = accountRepository;
+        this.transactionRepository = transactionRepository;
     }
 
     @Override
@@ -33,9 +40,33 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public AuthorizationResponse authorize(Account account, Transaction transaction) {
         // TODO:
-        // - Implementar as regras de negócio L1, L2 e L3
-        // - Persistir as transactions no banco e os novos saldos
-        return new AuthorizationResponse("01");
+        // - Implementar as regras de negócio L2 e L3
+        // - Usar enums para lidar com códigos de resposta e MCCs conhecidos
+        // - Usar @Transactional
+
+        BigDecimal transactionAmount = transaction.getAmount();
+
+        if (transactionAmount.compareTo(BigDecimal.ZERO) <= 0) {
+            return new AuthorizationResponse("07");
+        }
+
+        String mcc = transaction.getMcc();
+
+        Function<Account, BigDecimal> getter = BalanceMapper.getGetter(mcc);
+        BiConsumer<Account, BigDecimal> setter = BalanceMapper.getSetter(mcc);
+
+        BigDecimal balance = getter.apply(account);
+
+        if (balance.compareTo(transactionAmount) >= 0) {
+            setter.accept(account, balance.subtract(transactionAmount));
+
+            accountRepository.save(account);
+            transactionRepository.save(transaction);
+
+            return new AuthorizationResponse("01");
+        } else {
+            return new AuthorizationResponse("51");
+        }
     }
 
     @Override
