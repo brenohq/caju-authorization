6ac682793694d04eaac27ff73c343aba2b992bc7
brenohq <brenosc2@hotmail.com>
2024-07-11 17:19:20 -0300

    modifying mapper to work with enums

--- src/main/java/com/brenohq/caju_authorization/mapper/BalanceMapper.java
+++ src/main/java/com/brenohq/caju_authorization/mapper/BalanceMapper.java
@@ -1,5 +1,6 @@
 package com.brenohq.caju_authorization.mapper;
 
+import com.brenohq.caju_authorization.constant.MccEnum;
 import com.brenohq.caju_authorization.model.Account;
 
 import java.math.BigDecimal;
@@ -9,34 +10,26 @@ import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 public class BalanceMapper {
-    private static final Map<String, Function<Account, BigDecimal>> balanceGetters = new HashMap<>();
-    private static final Map<String, BiConsumer<Account, BigDecimal>> balanceSetters = new HashMap<>();
+    private static final Map<MccEnum, Function<Account, BigDecimal>> balanceGetters = new HashMap<>();
+    private static final Map<MccEnum, BiConsumer<Account, BigDecimal>> balanceSetters = new HashMap<>();
 
     static {
         // Mapping getters to each MCC
-        balanceGetters.put("5411", Account::getFoodBalance);
-        balanceGetters.put("5412", Account::getFoodBalance);
-        balanceGetters.put("5811", Account::getMealBalance);
-        balanceGetters.put("5812", Account::getMealBalance);
-
-        // Defaults to cash balance
-        balanceGetters.put("default", Account::getCashBalance);
+        balanceGetters.put(MccEnum.FOOD, Account::getFoodBalance);
+        balanceGetters.put(MccEnum.MEAL, Account::getMealBalance);
+        balanceGetters.put(MccEnum.DEFAULT, Account::getCashBalance);
 
         // Mapping setters to each MCC
-        balanceSetters.put("5411", Account::setFoodBalance);
-        balanceSetters.put("5412", Account::setFoodBalance);
-        balanceSetters.put("5811", Account::setMealBalance);
-        balanceSetters.put("5812", Account::setMealBalance);
-
-        // Defaults to cash balance
-        balanceSetters.put("default", Account::setCashBalance);
+        balanceSetters.put(MccEnum.FOOD, Account::setFoodBalance);
+        balanceSetters.put(MccEnum.MEAL, Account::setMealBalance);
+        balanceSetters.put(MccEnum.DEFAULT, Account::setCashBalance);
     }
 
     public static Function<Account, BigDecimal> getGetter(String mcc) {
-        return balanceGetters.getOrDefault(mcc, balanceGetters.get("default"));
+        return balanceGetters.get(MccEnum.fromMcc(mcc));
     }
 
     public static BiConsumer<Account, BigDecimal> getSetter(String mcc) {
-        return balanceSetters.getOrDefault(mcc, balanceSetters.get("default"));
+        return balanceSetters.get(MccEnum.fromMcc(mcc));
     }
 }
